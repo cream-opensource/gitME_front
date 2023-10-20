273d4eea66b71adcf51c554b60555615e253fd62
hoon-hoon <kc0393@naver.com>
2023-10-20 16:13:37 +0900

    기본 세팅
    기본적인 디렉토리 세팅 및 drawer

--- gitme/lib/main.dart
+++ gitme/lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:gitme/shared/menu_bottom.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:gitme/provider/pixabay_photos.dart';
+import 'package:gitme/screens/flutter_screen.dart';
+import 'package:gitme/screens/login_screen.dart';
+import 'package:gitme/screens/main_screen.dart';
+import 'package:gitme/screens/profile_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'gitME',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MenuBottom(),
-    );
+        title: 'Drawer without AppBar',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FlutterScreen(),
+        routes: {
+          MainScreen.route: (_) => MainScreen(),
+          FlutterScreen.route: (_) => FlutterScreen(),
+          ProfileScreen.route: (_) => ProfileScreen()
+        },
+      );
   }
 }
---
+++ gitme/lib/widgets/main_drawer.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:gitme/screens/flutter_screen.dart';
+import 'package:gitme/screens/login_screen.dart';
+import 'package:gitme/screens/main_screen.dart';
+import 'package:gitme/screens/profile_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: Image.asset(
+                  'assets/profile_bg_01.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(bottom: 10),
+                width: 100,
+                height: 100,
+                child: Image.asset('assets/default-user.png'),
+              )
+            ],
+          ),
+          SizedBox(height: 30),
+          ListTile(
+            onTap: () => Navigator.pushReplacementNamed(
+              context,
+              MainScreen.route,
+            ),
+            leading: Icon(Icons.account_circle),
+            title: Text(
+              'home',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushReplacementNamed(
+              context,
+              ProfileScreen.route,
+            ),
+            leading: Icon(Icons.account_circle),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushReplacementNamed(
+              context,
+              FlutterScreen.route,
+            ),
+            leading: Icon(Icons.account_circle),
+            title: Text(
+              'flutter',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
