89a179886329371b42fce7ee95a27adf44d92d91
hoon-hoon <kc0393@naver.com>
2023-11-12 18:14:27 +0900

    스플래시 오류 개선중 / 회원가입 부분 마크업 및 validation, 통신 부분 구현

--- gitme/lib/screens/login_screen.dart
+++ gitme/lib/screens/login_screen.dart
@@ -2,27 +2,45 @@ import 'package:gitme/screens/main_screen.dart';
 import 'package:gitme/widgets/custom_drawer_btn.dart';
 import 'package:gitme/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginScreen extends StatelessWidget {
+  Future<void> simulateLogin() async {
+    final response = await http
+        .get(Uri.parse('https://0b22-210-206-182-220.ngrok-free.app/login'));
+
+    if (response.statusCode == 200) {
+      print('데이터: ${response.body}');
+    } else {
+      print('요청 실패: ${response.reasonPhrase}');
+    }
+  }
+
   static final route = 'login-screen';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null, // AppBar를 제거
+      backgroundColor: const Color(0xff000E1B),
       body: Column(children: [
         Padding(padding: EdgeInsets.only(top: 150)),
         Center(
           child: Image(
-            image: AssetImage('assets/cream.png'),
+            image: AssetImage('assets/gitme_logo.png'),
             width: 170.0,
           ),
         ),
-        SizedBox(height: 40,),
+        SizedBox(
+          height: 40,
+        ),
         ButtonTheme(
             child: ElevatedButton(
               onPressed: () =>
-                  Navigator.pushReplacementNamed(context, MainScreen.route),
+              {
+                simulateLogin(),
+                Navigator.pushReplacementNamed(context, MainScreen.route),
+              },
               child: Text("Kakao Login"),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.yellowAccent,
@@ -33,8 +51,7 @@ class LoginScreen extends StatelessWidget {
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            )
-        )
+            ))
       ]),
       drawer: MainDrawer(),
     );
