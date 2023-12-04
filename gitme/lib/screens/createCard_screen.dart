80314ed0ef640e1c39af1f002615cbb80f0bcc02
hyunoi <nhe0622@gmail.com>
2023-12-04 15:31:28 +0900

    feat: edit createCard_screen

--- gitme/lib/screens/createCard_screen.dart
+++ gitme/lib/screens/createCard_screen.dart
@@ -1,14 +1,37 @@
-import 'package:flutter/material.dart';
+import 'dart:collection';
 
-class CreateCardScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
+
+class CreateCardScreen extends StatelessWidget {// State 클래스 추가
+  bool isListView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create Card'),
+        backgroundColor: Color(0xFFF4F4F4),
+        leading: IconButton(
+          icon: Image.asset(
+            'assets/back_button.png', // 이미지 경로
+            width: 20, // 이미지 너비
+            height: 20, // 이미지 높이
+          ),
+          onPressed: () {
+            Navigator.of(context).pushNamed(MainScreen.route);
+          },
+        ),
+        title: Text(
+          '명함 만들기',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('This is the CreateCard Screen'),
+      body: Column(
+        children: [
+          Text("hi")
+        ],
       ),
     );
   }
