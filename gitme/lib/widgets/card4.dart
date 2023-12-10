0842dbfd079139cd526ebaf4a9f7118794356e1f
hyunoi <nhe0622@gmail.com>
2023-12-10 20:45:19 +0900

    feat: card4 반응형

--- gitme/lib/widgets/card4.dart
+++ gitme/lib/widgets/card4.dart
@@ -31,21 +31,18 @@ class BusinessCard4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
-      width: MediaQuery
-          .of(context)
-          .size
-          .width * 0.8,
-      height: 400,
+      width: double.infinity,
       margin: EdgeInsets.only(top: 30),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color(0xFFDBC3EA), // Pink color
-            Color(0xFFDBC3EA), // Pink color
-            Color(0xFFFFFFFF), // White color
-            Color(0xFFFFFFFF), // White color
-
+            Color(0xFFDBC3EA),
+            Color(0xFFDBC3EA),
+            Color(0xFFFFFFFF),
+            Color(0xFFFFFFFF),
           ],
           stops: [0.0, 0.6, 0.4, 1.0],
           begin: Alignment.topCenter,
@@ -62,9 +59,8 @@ class BusinessCard4 extends StatelessWidget {
         ],
       ),
       child: Padding(
-        padding: EdgeInsets.fromLTRB(25, 25, 25, 25),
+        padding: EdgeInsets.all(screenWidth * 0.08),
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -74,76 +70,81 @@ class BusinessCard4 extends StatelessWidget {
                 Image.asset(
                   'assets/jjj.png',
                   fit: BoxFit.cover,
-                  width: 40, // 원하는 너비로 조절
-                  height: 40, // 원하는 높이로 조절
+                  width: screenWidth * 0.1,
+                  height: screenWidth * 0.1,
                 ),
-                SizedBox(width: 10,),
-                Text("m-a-king", style: TextStyle(
-                    fontSize: 34, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: 'DarkerGrotesque'
+                SizedBox(width: screenWidth * 0.02),
+                Text(
+                  "m-a-king",
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.08,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'DarkerGrotesque',
                   ),
-                )
+                ),
               ],
             ),
-            SizedBox(height: 10),
-
-            Text("멋진 청년.\n휼륭한 웹 개발자가 되기 위한 수련 중.\nI am 조재중이에요 ~", style: TextStyle(
-                fontSize: 14, color: Colors.white, fontFamily: 'DarkerGrotesque'),
-            ),
-            SizedBox(height: 50,),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("tistory"),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFD2AFE3),
-                      onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13), // 모서리 조절
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("notion"),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFD2AFE3),
-                      onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13), // 모서리 조절
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("기타"),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFD2AFE3),
-                      onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13), // 모서리 조절
-                      ),
-                    ),
-                  ),
-                  // Add more buttons as needed
-                ],
+            SizedBox(height: screenWidth * 0.01),
+            Text(
+              "멋진 청년.\n휼륭한 웹 개발자가 되기 위한 수련 중.\nI am 조재중이에요 ~",
+              style: TextStyle(
+                fontSize: screenWidth * 0.03,
+                color: Colors.white,
+                fontFamily: 'DarkerGrotesque',
               ),
             ),
-            SizedBox(height: 30,),
-            Text("Stack", style: TextStyle(color: Color(0xFF8B8B91), fontSize: 13)),
+            SizedBox(height: screenWidth * 0.05),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("tistory"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFD2AFE3),
+                        onPrimary: Colors.white,
+                        textStyle: TextStyle(fontSize: screenWidth * 0.03),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(screenWidth * 0.03),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: screenWidth * 0.01),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("notion"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFD2AFE3),
+                        onPrimary: Colors.white,
+                        textStyle: TextStyle(fontSize: screenWidth * 0.03),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(screenWidth * 0.03),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: screenWidth * 0.01),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("기타"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFD2AFE3),
+                        onPrimary: Colors.white,
+                        textStyle: TextStyle(fontSize: screenWidth * 0.03),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(screenWidth * 0.03),
+                        ),
+                      ),
+                    ),
+                    // Add more buttons as needed
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: screenWidth * 0.02),
+            Text("Stack", style: TextStyle(color: Color(0xFF8B8B91), fontSize: screenWidth * 0.03)),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
@@ -154,41 +155,35 @@ class BusinessCard4 extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFD2AFE3),
                       onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
+                      textStyle: TextStyle(fontSize: screenWidth * 0.03),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // 모서리 조절
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
                       ),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  SizedBox(width: screenWidth * 0.01),
                   ElevatedButton(
                     onPressed: () {},
                     child: Text("Node.js"),
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFD2AFE3),
                       onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
+                      textStyle: TextStyle(fontSize: screenWidth * 0.03),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // 모서리 조절
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
                       ),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  SizedBox(width: screenWidth * 0.01),
                   ElevatedButton(
                     onPressed: () {},
                     child: Text("Kotlin"),
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFD2AFE3),
                       onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
+                      textStyle: TextStyle(fontSize: screenWidth * 0.03),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // 모서리 조절
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
                       ),
                     ),
                   ),
@@ -196,8 +191,8 @@ class BusinessCard4 extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 8,),
-            Text("Info", style: TextStyle(color: Color(0xFF8B8B91), fontSize: 13)),
+            SizedBox(height: screenWidth * 0.008),
+            Text("Info", style: TextStyle(color: Color(0xFF8B8B91), fontSize: screenWidth * 0.03)),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
@@ -209,13 +204,13 @@ class BusinessCard4 extends StatelessWidget {
                         Text(
                           "Commit",
                           style: TextStyle(
-                            fontSize: 10,
+                            fontSize: screenWidth * 0.02,
                           ),
                         ),
                         Text(
                           "369",
                           style: TextStyle(
-                            fontSize: 14,
+                            fontSize: screenWidth * 0.03,
                           ),
                         ),
                       ],
@@ -223,15 +218,12 @@ class BusinessCard4 extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFD2AFE3),
                       onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // 모서리 조절
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
                       ),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  SizedBox(width: screenWidth * 0.01),
                   ElevatedButton(
                     onPressed: () {},
                     child: Column(
@@ -239,13 +231,13 @@ class BusinessCard4 extends StatelessWidget {
                         Text(
                           "Stars",
                           style: TextStyle(
-                            fontSize: 10,
+                            fontSize: screenWidth * 0.02,
                           ),
                         ),
                         Text(
                           "25",
                           style: TextStyle(
-                            fontSize: 14,
+                            fontSize: screenWidth * 0.03,
                           ),
                         ),
                       ],
@@ -253,15 +245,12 @@ class BusinessCard4 extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFD2AFE3),
                       onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // 모서리 조절
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
                       ),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  SizedBox(width: screenWidth * 0.01),
                   ElevatedButton(
                     onPressed: () {},
                     child: Column(
@@ -269,13 +258,13 @@ class BusinessCard4 extends StatelessWidget {
                         Text(
                           "PR",
                           style: TextStyle(
-                            fontSize: 10,
+                            fontSize: screenWidth * 0.02,
                           ),
                         ),
                         Text(
                           "104",
                           style: TextStyle(
-                            fontSize: 14,
+                            fontSize: screenWidth * 0.03,
                           ),
                         ),
                       ],
@@ -283,11 +272,8 @@ class BusinessCard4 extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFD2AFE3),
                       onPrimary: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // 모서리 조절
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
                       ),
                     ),
                   ),
@@ -296,24 +282,8 @@ class BusinessCard4 extends StatelessWidget {
               ),
             ),
           ],
-
-
         ),
       ),
     );
   }
-}
-
-
-Widget _buildRoundedText(String text) {
-  return Container(
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(30),
-      border: Border.all(color: Colors.white), // 테두리 색상 설정
-    ),
-    padding: EdgeInsets.fromLTRB(10, 8, 10, 8), // 텍스트와 테두리 간격 설정
-    child: Center(
-      child: Text(text, style: TextStyle(color: Colors.white, fontSize: 15)),
-    ),
-  );
 }
