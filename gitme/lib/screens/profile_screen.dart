63599baece0f930c38161a863f92f2388a4cae31
hyunoi <nhe0622@gmail.com>
2023-12-12 04:50:31 +0900

    feat: profile_screen

--- gitme/lib/screens/profile_screen.dart
+++ gitme/lib/screens/profile_screen.dart
@@ -23,15 +23,15 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final userData = Provider.of<UserData>(context, listen: false);
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFF4F4F4),
         leading: IconButton(
           icon: Image.asset(
-            'assets/back_button.png', // 이미지 경로
-            width: 20, // 이미지 너비
-            height: 20, // 이미지 높이
+            'assets/back_button.png',
+            width: 20,
+            height: 20,
           ),
           onPressed: () {
             Navigator.of(context).pushNamed(MainScreen.route);
@@ -44,95 +44,177 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      'jjj.png', // 이미지 경로
-                      width: 50, // 이미지 너비
-                      height: 50, // 이미지 높이
-                    ),
-                    SizedBox(width: 5), // 이미지와 텍스트 사이 간격 조정
-                    Text(
-                      '조재중',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color(0xFF676A66),
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                TextButton(
-                  onPressed: () {
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double screenWidth = MediaQuery.of(context).size.width;
+          final userData = Provider.of<UserData>(context, listen: false);
 
-                  },
-                  child: Row(
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(screenWidth * 0.06),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: screenWidth * 0.05),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Consumer<UserData>(
+                          builder: (context, userData, child) {
+                            return userData.avatarUrl != null
+                                ? Container(
+                              width: screenWidth * 0.4,
+                              height: screenWidth * 0.4,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.black12,
+                                  width: 2.0,
+                                ),
+                              ),
+                              child: ClipOval(
+                                child: Image.network(
+                                  userData.avatarUrl!,
+                                  width: screenWidth * 0.4,
+                                  height: screenWidth * 0.4,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            )
+                                : Container();
+                          },
+                        ),
+                        SizedBox(width: screenWidth * 0.03),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Consumer<UserData>(
+                                builder: (context, userData, child) {
+                                  return Text(
+                                    userData.name ?? '',
+                                    style: TextStyle(
+                                        fontSize: screenWidth * 0.06,
+                                        fontWeight: FontWeight.w800
+                                    ),
+                                  );
+                                },
+                              ),
+                              Row(
+                                children: [
+                                  Image.asset(
+                                    'assets/git_icon.png',
+                                    width: screenWidth * 0.035,
+                                    height: screenWidth * 0.035,
+                                  ),
+                                  SizedBox(width: screenWidth * 0.012,),
+                                  Consumer<UserData>(
+                                    builder: (context, userData, child) {
+                                      return Text(
+                                        userData.nickname ?? '',
+                                        style: TextStyle(
+                                            fontSize: screenWidth * 0.035,
+                                            fontWeight: FontWeight.w500
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  // 수정 버튼 눌렸을 때의 동작
+                                },
+                                child: Text(
+                                  '내 정보 수정하기',
+                                  style: TextStyle(
+                                    fontSize: screenWidth * 0.03,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Color(0xFF939393),
+                                  padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                ),
+                              ),
+                            ]
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: screenWidth * 0.01),
+                  ListView(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
                     children: [
-                      Text(
-                        '수정',
-                        style: TextStyle(
-                          color: Color(0xFF676A66),
-                          fontSize: 13,
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.person),
+                          title: Text(userData.introduce ?? ''),
                         ),
                       ),
-                      SizedBox(width: 5),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.link),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: userData.externalLink?.entries.map((entry) {
+                              return Text('${entry.key}: ${entry.value}');
+                            }).toList() ?? [],
+                          ),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.language),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: userData.skill?.entries.expand((entry) {
+                              return [
+                                Text(entry.key),
+                                Text(entry.value),
+                                SizedBox(height: 8),
+                              ];
+                            }).toList() ?? [],
+                          ),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.cake_outlined),
+                          title: Text(userData.birthDate ?? ''),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.email_outlined),
+                          title: Text(userData.email ?? ''),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.phone),
+                          title: Text(userData.phone ?? ''),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.cake_outlined),
+                          title: Text(userData.birthDate ?? ''),
+                        ),
+                      ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 30.0),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.9,
-            child: Column(
-              children: <Widget>[
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.person_outline),
-                    title: Text('Hoon'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('your@email.com'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.phone),
-                    title: Text('000-0000-0000'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.favorite),
-                    title: Text('1998.12.11'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    onTap: _logout,
-                    leading: Icon(Icons.logout),
-                    title: Text('로그아웃'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          );
+        },
       ),
-      drawer: MainDrawer(),
 
+      drawer: MainDrawer(),
     );
   }
 
